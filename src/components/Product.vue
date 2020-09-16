ea7bc06639093dbaa6ed90066123ae0bec78a9c1
stephan <stephanmh@gmail.com>
2020-09-16 18:49:56 +0200

    scss and more.

---
+++ src/components/Product.vue
@@ -0,0 +1,33 @@
+<template>
+  <li class="product">
+    <div class="top-info">
+      <div class="check"></div>
+      <img src="/img/product.png" alt="product image">
+      <h3>Sumsung Series 6 J6300</h3>
+    </div> <!-- .top-info -->
+
+    <ul class="cd-features-list">
+      <li>$600</li>
+      <li class="rate"><span>5/5</span></li>
+      <li>1080p</li>
+      <li>LED</li>
+      <li>47.6 inches</li>
+      <li>800Hz</li>
+      <li>2015</li>
+      <li>mpeg4</li>
+      <li>1 Side</li>
+      <li>3 Port</li>
+      <li>1 Rear</li>
+    </ul>
+  </li> <!-- .product -->
+</template>
+
+<script>
+export default {
+name: "Product"
+}
+</script>
+
+<style scoped>
+
+</style>
